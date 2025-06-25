8bc008929524c2d3b30f7fcbe7064283a27abc59
ciciaashadiqah <144694654+ciciaashadiqah@users.noreply.github.com>
2025-06-25 16:55:41 +0700

    teori pbo tugas kelompok

---
+++ Tugas1/Rectangle.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas1;
+
+/**
+ *
+ * @author USER
+ */
+public class Rectangle extends Shape {
+    private double width = 1.0;
+    private double length = 1.0;
+
+    public Rectangle() {}
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle[width=" + width + ", length=" + length + ", area=" + getArea() + ", perimeter=" + getPerimeter() + ", " + super.toString() + "]";
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
