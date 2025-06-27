cfeb2e7719337d41b8d367dca9aa745da0515712
ciciaashadiqah <144694654+ciciaashadiqah@users.noreply.github.com>
2025-06-27 12:47:34 +0700

    teori pbo

---
+++ TugasKelompokTeoriAsiahDanNabila/src/Tugas3/Rectangle.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas3;
+
+/**
+ *
+ * @author USER
+ */
+public class Rectangle implements GeometricObject {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle[width=" + width + ", length=" + length + "]";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
